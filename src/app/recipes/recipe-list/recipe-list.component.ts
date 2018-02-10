89f253b14cc8ed1c84749844e098778f20b1cca6
Spenser Hardin <shardin@pillartechnology.com>
2018-02-10 09:26:02 -0600

    Adding Recipe Details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -9,9 +9,12 @@ import {Recipe} from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test recipe', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg'),
     new Recipe('A Test Recipe', 'This is a test recipe', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg')
   ];
-  constructor() { }
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
