a1f7c09ddd88088bbe8cde39c3ca547d62168aa6
Spenser Hardin <shardin@pillartechnology.com>
2018-02-10 08:33:58 -0600

    Adding stubs to later fill in with model

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-recipes',
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesComponent implements OnInit {
 
-  recipes = [];
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
