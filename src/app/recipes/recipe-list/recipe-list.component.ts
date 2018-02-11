08db53ee7aa8ea6f08ded38bc4b56f07d2203e11
Spenser Hardin <shardin@pillartechnology.com>
2018-02-10 18:16:06 -0600

    Adding event handling to recipe details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -8,9 +8,10 @@ import {Recipe} from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() recipeSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a test recipe', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg'),
-    new Recipe('A Test Recipe', 'This is a test recipe', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg')
+    new Recipe('Rosemary Salmon', 'This is a test recipe', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg'),
+    new Recipe('Roadside Chicken', 'This is a test recipe', 'https://static01.nyt.com/images/2017/06/07/dining/07INDIAjp7/07INDIAjp7-articleLarge.jpg')
   ];
 
   constructor() {
@@ -19,4 +20,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
+  }
 }
